bcf70abd48025792b9bf1d3152fb80d31c1c6464
Bryan Ramos <br1173626@gmail.com>
2026-04-06 12:05:10 -0400

    .

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,7 +7,8 @@ from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """Coroutine that yields a random number between 0 and 10, 10 times."""
+    """Coroutine that yields a random number
+    between 0 and 10, 10 times."""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
