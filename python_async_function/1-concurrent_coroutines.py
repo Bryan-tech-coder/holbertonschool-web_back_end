6fde8b4afe5f0c3330e4033205c0045e5f34e0df
Bryan Ramos <br1173626@gmail.com>
2026-04-06 11:37:57 -0400

    /

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Module that contains an async function to run multiple wait_random coroutines concurrently."""
+"""Module to concurrently run wait_random coroutines and return results in order of completion."""
 
 import asyncio
 from 0-basic_async_syntax import wait_random
@@ -7,10 +7,17 @@ from typing import List
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawn wait_random n times with max_delay and return list of results in order of completion."""
+    """
+    Spawn wait_random n times with the specified max_delay and
+    return a list of all delays in order of completion.
+    """
+    # Crear tareas
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    results = []
+    results: List[float] = []
+
+    # Iterar sobre las tareas en orden de finalización
     for task in asyncio.as_completed(tasks):
         result = await task
         results.append(result)
+
     return results
