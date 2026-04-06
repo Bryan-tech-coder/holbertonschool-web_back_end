d37cd572854c6ae4dd7d999c10db8d01a1d8f134
Bryan Ramos <br1173626@gmail.com>
2026-04-06 11:04:30 -0400

    .

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Module that contains a function to sum a mixed list of integers and floats."""
+"""Module that contains a function to 
+sum a mixed list of integers and floats."""
 
 from typing import List, Union
 
