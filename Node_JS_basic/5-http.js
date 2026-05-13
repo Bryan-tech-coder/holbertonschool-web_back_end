abdba7991547348051dc82c01ef534a7e94db4aa
Bryan Ramos <br1173626@gmail.com>
2026-05-13 12:29:56 -0400

    http

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -3,60 +3,56 @@ const fs = require('fs');
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
       if (err) {
         reject(new Error('Cannot load the database'));
         return;
       }
 
-      const lines = data
-        .trim()
-        .split('\n')
-        .filter((line) => line.trim() !== '');
+      const lines = data.trim().split('\n');
+      const students = lines.slice(1).filter((line) => line.trim() !== '');
 
-      const students = lines.slice(1);
-
-      let output = `Number of students: ${students.length}`;
+      const result = [];
+      result.push(`Number of students: ${students.length}`);
 
       const fields = {};
-
       students.forEach((line) => {
         const [firstname, , , field] = line.split(',');
-
         if (!fields[field]) {
           fields[field] = [];
         }
-
         fields[field].push(firstname);
       });
 
       for (const field in fields) {
-        output += `\nNumber of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
+        if (Object.prototype.hasOwnProperty.call(fields, field)) {
+          result.push(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+        }
       }
 
-      resolve(output);
+      resolve(result.join('\n'));
     });
   });
 }
 
-const app = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/plain');
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
 
-  if (request.url === '/') {
-    response.end('Hello Holberton School!');
-  } else if (request.url === '/students') {
-    response.write('This is the list of our students\n');
-
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
     countStudents(process.argv[2])
       .then((data) => {
-        response.end(data);
+        res.end(data);
       })
-      .catch((error) => {
-        response.end(error.message);
+      .catch(() => {
+        res.end('Cannot load the database');
       });
   } else {
-    response.end('Hello Holberton School!');
+    res.statusCode = 404;
+    res.end();
   }
 });
 
